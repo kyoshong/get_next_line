4ddc24abc7537019169c9a526c4b28745a93c2b1
kyoshong <qkrgytjd0827@naver.com>
2021-02-05 01:34:23 +0900

    leak

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hyospark <hyospark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 18:54:03 by hyospark          #+#    #+#             */
-/*   Updated: 2021/02/04 20:43:27 by hyospark         ###   ########.fr       */
+/*   Updated: 2021/02/05 01:24:21 by hyospark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ char	*re_backup(char *cont)
 	while (cont[i] != '\n' && cont[i])
 		i++;
 	if (!cont[i])
-		return (0);
+		return (NULL);
 	len_c = ft_strlen(cont);
 	if (!(temp = malloc(sizeof(char) * (len_c - i) + 1)))
 		return (NULL);
@@ -54,45 +54,51 @@ int		line_check(char *cont)
 
 int get_next_line(int fd, char **line)
 {
-	static char	*cont[FOPEN_MAX];
+	static char	*cont[OPEN_MAX];
 	char		buf[BUFFER_SIZE + 1];
 	ssize_t		check;
+	char		*temp;
 
 	if (fd < 0 || !line || BUFFER_SIZE <= 0)
 		return (-1);
 	check = 1;
-	while (line_check(cont[fd]) == 0 && check != 0)
+	while (!line_check(cont[fd])&& check != 0)
 	{
 		if ((check = read(fd, buf, BUFFER_SIZE)) < 0)
 			return (-1);
 		buf[check] = '\0';
-		//printf("buf :%s\n", buf);
-		//printf("before cont :%s\n", cont[fd]);
-		cont[fd] = ft_strjoin(cont[fd], buf);
-		//printf("after cont :%s\n", cont[fd]);
-		buf[0] = '\0';
+		temp = ft_strjoin(cont[fd], buf);
+		if (cont[fd])
+			free(cont[fd]);
+		cont[fd] = temp;
 	}
 	*line = ft_strdup(cont[fd]);
-	//printf("line :%s", *line);
 	cont[fd] = re_backup(cont[fd]);
 	if (check == 0)
+	{
+		free(cont[fd]);
+		cont[fd] = NULL;
 		return (0);
+	}
 	return (1);
 }
 
-// int	main()
-// {
-// 	int		ret;
-// 	int		fd;
-// 	char	*line;
+int	main()
+{
+	int		ret;
+	int		fd;
+	char	*line;
 
-// //*line이 선언된 상태로 get_next_line 함수로 넘어감
+//*line이 선언된 상태로 get_next_line 함수로 넘어감
 
-// 	fd = open("text.txt", O_RDONLY);
+	fd = open("text.txt", O_RDONLY);
 
-// 	while ((ret = (get_next_line(fd, &line)) > 0))
-// 	{
-// 		printf("-------ww---------");
-// 		printf("line : %s", line);
-// 	}
-// }
+	while ((ret = (get_next_line(fd, &line)) > 0))
+	{
+		printf("-------ww---------");
+		//printf("line : %s", line);
+		printf("%d\n", ret);
+	}
+	printf("%d\n", ret);
+		system("leaks a.out > leaks_result; cat leaks_result | grep leaked && rm -rf leaks_result");
+}
